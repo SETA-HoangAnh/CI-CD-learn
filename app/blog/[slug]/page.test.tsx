f27d099a0fdcb9c02c74211ca2d8f85a2b05f751
hoanganh <hoanganhnguyen31122002@gmail.com>
2025-09-18 13:57:08 +0700

    blablabla

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -10,7 +10,7 @@ it("App Router: Works with dynamic route segments", () => {
   expect(screen.getByRole("heading")).toHaveTextContent("Slug: Test");
 });
 
-it('HoleTex should be in the document', () =>{
+it('HoleTe should be in the document', () =>{
   render(<Page params={{ slug: "Test" }} />);
   expect(screen.getByText("HoleTe")).toBeInTheDocument();
 })
